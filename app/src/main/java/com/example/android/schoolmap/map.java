74394c359b50f3ac034d1329f7ef7cc25284bedf
ee5415 <qorentks7777@gmail.com>
2016-07-18 16:25:46 +0800

    make more contents

--- app/src/main/java/com/example/android/schoolmap/map.java
+++ app/src/main/java/com/example/android/schoolmap/map.java
@@ -1,6 +1,7 @@
 package com.example.android.schoolmap;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,5 +19,15 @@ public class map extends AppCompatActivity {
     {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void detail (View view) {
+        goToUrl("http://www.dankook.ac.kr/web/kor/campusmap?p_p_id=Campus_WAR_campusportlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_Campus_WAR_campusportlet_sCampusId=1&_Campus_WAR_campusportlet_pageView=detail&_Campus_WAR_campusportlet_action=view");
     }
 }
